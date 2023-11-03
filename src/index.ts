0642fc8d7d9cc1165768660aff2db8ff591d5d4f
Puiyee <Pyeewong11@gmail.com>
2023-11-03 18:19:07 +0800

    feat(ts): 利用陣列記錄待辦事項

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import (Todo) from"./todo';
 
-const todo = new Todo(1,'期中考作業');
-const todo = new Todo(2,'期末考作業');
+const todo = [
+    new Todo(1,'期中考作業'),
+    new Todo(2,'期中考作業'),
+    new Todo(3,'期中考作業'),
+];
 
 console.log("To Do:",todo);
 console.table(todo);
