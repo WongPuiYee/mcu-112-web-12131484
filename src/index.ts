50f3d3fc61b5d6d56f645ad766e53deece06247a
Puiyee <Pyeewong11@gmail.com>
2023-11-03 18:23:15 +0800

    feat(ts): 利用函式設定待辦事項已完成

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,11 @@ const todo = [
     new Todo(3,'期中考作業'),
 ];
 
+setTaskFinished(todos[0],new Date());
 console.log("To Do:",todo);
 console.table(todo);
+
+function setTaskFinished(todo: Todo,finishDate: Date): void{
+    todo.hasFinished = true;
+    todo.finishDate = finishDate;
+}
