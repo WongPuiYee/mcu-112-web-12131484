110f5ee8bb71e8960b53cf9612342f9f1ab335b4
Puiyee <Pyeewong11@gmail.com>
2023-11-24 21:01:51 +0800

    feat(footer): 實作待辦事項頁尾元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { TodoComponent } from "./todo/todo.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, TodoComponent],
+  imports: [HeaderComponent, TodoComponent, FooterComponent],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
