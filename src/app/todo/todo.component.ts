ef3902fea134817cf632d46b06892f1c48a3e694
Puiyee <Pyeewong11@gmail.com>
2023-11-24 21:30:24 +0800

    feat(todo): 傳入屬性變更成待辦事項型別

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -9,27 +9,16 @@ import { CommonModule, DatePipe } from "@angular/common";
   styleUrl: "./todo.component.css",
 })
 export class TodoComponent {
-  @Input({ required: true, transform: numberAttribute })
-  id!: number;
-
   @Input({ required: true })
-  content!: string;
+  task!: Todo;
 
-  @Input({ transform: booleanAttribute })
-  hasFinished!: boolean;
   @output()
-  readonly hasFinishedChange = new EventEmitter<boolean>();
-
-  @Input()
-  finishDate?: Date;
-  @Output()
-  readonly finishDateChange = new EventEmitter<Date | undefined>();
+  readonly stateChange = new EventEmitter<boolean>();
 
   @HostBinding("class")
   class = "app-todo";
 
   onSetStatus(hasFinished: boolean): void {
-    this.hasFinishedChange.emit(hasFinished);
-    this.finishDateChange.emit(hasFinished ? new Date() : undefined);
+    this.stateChange.emit(hasFinished);
   }
 }
