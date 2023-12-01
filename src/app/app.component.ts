7a75ca798650b334d5a7acb43d1fcf10dbce4df8
Puiyee <Pyeewong11@gmail.com>
2023-12-01 15:43:59 +0800

    feat(todo):實作顯示多筆待辦事項

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,14 @@ import { JsonPipe } from "@angular/common";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  tasks = [new Todo(1, "待辦事項A"), new Todo(2, "待辦事項B")];
+
+  onStateChange(task: {index: number, state: boolean}): void {
+    if (task, state) {
+      this.task[task.index].setFinished(new Date());
+    } else {
+      this.task[task.index].finishDate = undefined;
+      this.task[task.index].hasFinished = false;
+    }
+}
