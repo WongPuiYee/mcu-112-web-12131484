f97940204085a715d2eab3c8151a4ee892d783f3
Puiyee <Pyeewong11@gmail.com>
2023-12-01 15:55:24 +0800

    feat(todo-list):無待辦事項顯示實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,19 @@ import { JsonPipe } from "@angular/common";
 export class AppComponent {
   tasks = [new Todo(1, "待辦事項A"), new Todo(2, "待辦事項B")];
 
-  onStateChange(task: {index: number, state: boolean}): void {
-    if (task, state) {
+  onStateChange(task: { index: number; state: boolean }): void {
+    if ((task, state)) {
       this.task[task.index].setFinished(new Date());
     } else {
       this.task[task.index].finishDate = undefined;
       this.task[task.index].hasFinished = false;
     }
+  }
+  onSet(): void {
+    this.tasks = [new Todo(1, "待辦事項A"), new Todo(2, "待辦事項B")];
+  }
+
+  onClear(): void {
+    this.tasks = [];
+  }
 }
