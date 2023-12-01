37c83decfb17a7a5abf3fcbe6e24bdfb83549873
Puiyee <Pyeewong11@gmail.com>
2023-12-01 14:51:44 +0800

    feat(todo-list):建立工作事項清單元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,8 @@ import { JsonPipe } from "@angular/common";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, TodoComponent, FooterComponent, JsonPipe],
+  imports: [HeaderComponent, TodoListComponent, FooterComponent, JsonPipe],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  task = new Todo(1, "待辦事項 A");
-
-  onStateChange(state: boolean): void {
-    if (state) {
-      this.task.setFinished(new Date());
-    } else {
-      this.task.finishDate = undefined;
-      this.task.hasFinished = false;
-    }
-  }
-}
+export class AppComponent {}
