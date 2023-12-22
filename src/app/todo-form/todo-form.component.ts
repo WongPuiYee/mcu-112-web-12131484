d5c741ce210b790d0d4b89564756bd5b5576f3a3
Puiyee <Pyeewong11@gmail.com>
2023-12-22 18:53:32 +0800

    feat(todo-form): 待辦事項標籤設定作業實作

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,12 +1,18 @@
 import { Component, HostBinding } from "@angular/core";
 import { CommonModule, JsonPipe } from "@angular/common";
-import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 import { ITodoForm } from "../interface/todo-form .interface";
 
 @Component({
   selector: "app-todo-form",
   standalone: true,
-  imports: [NgIf, ReactiveFormsModule, JsonPipe],
+  imports: [NgIf, NgFor, ReactiveFormsModule, JsonPipe],
   templateUrl: "./todo-form.component.html",
   styleUrl: "./todo-form.component.css",
 })
@@ -21,15 +27,29 @@ export class TodoFormComponent {
     content: new FormControl<string | null>(null, {
       validators: [Validators.required],
     }),
+    tags: new FormArray<FormControl<string | null>>([]),
   });
 
   get formData(): Todo {
     return new Todo({
       content: this.content.value!,
+      tags: this.tags.value.map((tag) => tag!),
     });
   }
+
   get content(): FormControl<string | null> {
     return this.form.get("content") as FormControl<string | null>;
+  }
+
+  get tags(): FormArray<FormControl<string | null>> {
+    return this.form.get("tags") as FormArray<FormControl<string | null>>;
+  }
+
+  onAddTag(): void {
+    const control = new FormControl<string | null>(nul, {
+      validators: [Validators.required],
+    });
+    this.tags.push(control);
   }
 
   onSave(): void {
